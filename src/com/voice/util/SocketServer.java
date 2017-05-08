73593a41129aa1b11203046a7a18c89e660253a8
sunxiaohang <sxh13208803520@gmail.com>
2017-05-08 17:06:16 +0800

    socketserver

---
+++ src/com/voice/util/SocketServer.java
@@ -0,0 +1,57 @@
+package com.voice.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by sunxiaohang on 17-5-8.
+ */
+public class SocketServer extends Thread {
+    private Socket client;
+    private String controlcode = "";
+
+    public SocketServer(Socket c) {
+        this.client = c;
+    }
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    client.getInputStream()));
+            PrintWriter out = new PrintWriter(client.getOutputStream());
+            while (true) {
+                String str = in.readLine();
+                if (str == null) break;
+                System.out.println(str);
+                if (str.equals("110")) {
+                    String[] loadvideo = {"/bin/bash", "-c", "/home/pi/VideoStream.sh  >/dev/null 2>&1 &"};
+                    java.lang.Process process = Runtime.getRuntime().exec(loadvideo);
+                } else if (str.equals("119")) {
+                    String[] command1 = {"/bin/bash", "-c", "killall -9 mjpg_streamer"};
+                    String[] command2 = {"/bin/bash", "-c", "killall -9 video.sh"};
+                    java.lang.Process process1 = Runtime.getRuntime().exec(command1);
+                    java.lang.Process process2 = Runtime.getRuntime().exec(command2);
+                } else {
+                    controlcode = "echo -n " + str + " >/dev/ttyUSB0";
+                    String[] sendcode = {"/bin/bash", "-c", controlcode};
+                    java.lang.Process process = Runtime.getRuntime().exec(sendcode);
+                    out.println("has receive....");
+                    out.flush();
+                }
+            }
+            client.close();
+        } catch (IOException ex) {
+        } finally {
+        }
+    }
+    public static void startServer() throws IOException {
+        ServerSocket server = new ServerSocket(6789);
+        while (true) {
+            SocketServer mc = new SocketServer(server.accept());
+            mc.start();
+        }
+    }
+}
--- src/com/voice/view/Main.java
+++ src/com/voice/view/Main.java
@@ -2,13 +2,18 @@ package com.voice.view;
 
 import com.iflytek.cloud.speech.SpeechConstant;
 import com.iflytek.cloud.speech.SpeechUtility;
+import com.voice.util.SocketServer;
+import com.voice.util.VoiceAwake;
+
+import java.io.IOException;
 
 public class Main {
     private final static String APPID = "= 58f3097d";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpeechUtility.createUtility(SpeechConstant.APPID + APPID);//初始化语音识别
-//        TextToSpeech.getInstance().translate(TalkMessages.HELLO);//打招呼
-//        VoiceAwake.getInstance().running();//开启语音监听
+        TextToSpeech.getInstance().translate(TalkMessages.HELLO);//打招呼
+        VoiceAwake.getInstance().running();//开启语音监听
+        SocketServer.startServer();//开启socket服务器
         SpeechToText.getInstance().conformFace("./picture/result.png");
     }
 }
